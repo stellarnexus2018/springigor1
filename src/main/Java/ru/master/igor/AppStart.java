3090286d996000ed49a30befbce20cf86dccff8e
stellarnexus <stellarnexus@gmail.com>
2020-09-03 00:29:53 +0300

    тут изучение scope бинов

--- src/main/Java/ru/master/igor/AppStart.java
+++ src/main/Java/ru/master/igor/AppStart.java
@@ -9,12 +9,24 @@ public class AppStart {
     );
 
 
-    MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
+    MusicPlayer mp1 = context.getBean("musicPlayer", MusicPlayer.class);
+    MusicPlayer mp2 = context.getBean("musicPlayer", MusicPlayer.class);
+
+    System.out.println(mp1==mp2);
+    System.out.println(mp1);
+    System.out.println(mp2);
+
+    mp1.setName("tutu");
+
+    System.out.println(mp1.getName());
+    System.out.println(mp2.getName());
 
 
-    mp.PlaySound();
-    System.out.println(mp.getName());
-    System.out.println(mp.getVolume());
+
+    //mp.PlaySound();
+    //System.out.println(mp.getName());
+    //System.out.println(mp.getVolume());
+    //System.out.println(mp.isIs_now());
 
     context.close();
 
--- src/main/Java/ru/master/igor/MusicPlayer.java
+++ src/main/Java/ru/master/igor/MusicPlayer.java
@@ -2,22 +2,29 @@ package ru.master.igor;
 
 public class MusicPlayer {
   private IMusic imuse;
-
   private String name;
   private int volume;
+
+  public boolean isIs_now() {
+    return is_now;
+  }
+
+  public void setIs_now(boolean is_now) {
+    this.is_now = is_now;
+  }
+
+  private boolean is_now;
+
 
   public String getName() {
     return name;
   }
-
   public void setName(String name) {
     this.name = name;
   }
-
   public int getVolume() {
     return volume;
   }
-
   public void setVolume(int volume) {
     this.volume = volume;
   }
@@ -25,20 +32,11 @@ public class MusicPlayer {
   public MusicPlayer(IMusic imuse) {
     this.imuse = imuse;
   }
-
-
   public MusicPlayer(){};
 
   public void setImuse(IMusic imuse){this.imuse = imuse;}
 
   public void PlaySound(){
     System.out.println("Playing music: " + imuse.getSong());
-
-
-
-
   }
-
-
-
 }
